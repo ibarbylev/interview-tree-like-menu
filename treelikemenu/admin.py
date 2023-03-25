811149d05c889d4205b2c75c48b3311af1aad59e
ibarbylev <ibarbylev@gmail.com>
2023-03-25 18:05:26 +0200

    add models and migrations

--- treelikemenu/admin.py
+++ treelikemenu/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from treelikemenu.models import Menu, MenuItem
+
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+    list_display = ('name', 'slug')
+
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+    list_display = ('name', 'menu', 'parent', 'slug')
+
+
+
--- treelikemenu/models.py
+++ treelikemenu/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Menu(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name=_('menu name'))
+    slug = models.SlugField(max_length=100, verbose_name=_('menu name for url'))
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = _('Menu')
+        verbose_name_plural = _('Menus')
+
+    def __str__(self):
+        return self.name
+
+
+class MenuItem(models.Model):
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE, verbose_name=_('menu name'))
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, verbose_name=_('submenu'))
+    name = models.CharField(max_length=100, verbose_name=_('menu item name'))
+    slug = models.SlugField(max_length=100, verbose_name=_('menu item name for url'))
+
+    class Meta:
+        ordering = ['pk']
+        verbose_name = _('MenuItem')
+        verbose_name_plural = _('MenuItems')
+
+    def __str__(self):
+        return f'{self.name} - {self.menu} - {self.parent}'
